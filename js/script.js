f1b7c39617492400d076fb7e0d452b1aa6598995
L kabeer <ahmed@Lkabeer.com>
2017-10-22 10:28:20 +0200

    version 1.6

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 // JavaScript Document
-window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: 'e87ada04-74d6-4b98-b10a-f2ba969f17a6', f: true }); done = true; } }; })();
+// chat X-Team Start
+//window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: 'e87ada04-74d6-4b98-b10a-f2ba969f17a6', f: true }); done = true; } }; })();
+// chat X-Team End
+
+// Moving Monuments X-Team Start
+ 	$('#webTicker').webTicker();
+// Moving Monuments X-Team end
